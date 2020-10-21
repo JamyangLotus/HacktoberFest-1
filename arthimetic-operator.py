3937574ce0ff477bfe5748a3c2650c4f7b698f33
Jamyang Lotus <lotusjamyang@gmail.com>
2020-10-21 11:51:50 +0530

    Arithmetic operator updated

--- arthimetic-operator.py
+++ arthimetic-operator.py
@@ -1,7 +1,8 @@
+print("Hello my friend Today we are going to show different arithmetic operators in python")
 x = int(input("What is the first number? "))
 y = int(input("What is the second number? "))
  
-print("The sum is: ", x+y)
-print("The difference is: ", x-y)
-print("The product is: ", x*y)
-print("The quotient is: ", x//y)
+print("The sum is: ", x+y) //addition
+print("The difference is: ", x-y) //Difference
+print("The product is: ", x*y) //Multiplication
+print("The quotient is: ", x//y) //integer quotient
